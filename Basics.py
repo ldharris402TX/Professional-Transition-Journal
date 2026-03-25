a114bf70e2f3b3e48d86ec726d926ace3cb714c7
LaKendra D Randolph <kendra@lakendras-air.lan>
2026-03-25 17:47:41 -0500

    Day 77:Started from the basics. Mastered variable assignment.

---
+++ Basics.py
@@ -0,0 +1,5 @@
+#Variables Practice
+tenant_name = "Kendra Randolph"
+unit_zipcode = 78701
+monthly_rent = 2450.50
+print(f"Good Day {tenant_name}, in zipcode {unit_zipcode} your monthly rent is {monthly_rent}.")
