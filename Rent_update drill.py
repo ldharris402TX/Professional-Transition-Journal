3318f193a0acc8bd4f0e4954da0016f742e422ce
LaKendra D Randolph <kendra@lakendras-air.lan>
2026-03-25 18:09:58 -0500

    Day 77:Mastered variable math and re-assignment.

---
+++ Rent_update drill.py
@@ -0,0 +1,10 @@
+#RE drills
+property_name = "Austin Heights Unit A"
+current_rent = 2100
+rent_increase = 125
+new_rent = current_rent + rent_increase
+#Property Status
+prop_status1 = "Occupied"
+prop_status2 = "Lease Renewal Pending"
+
+print(f"Property: {property_name} Your previous monthly rent {current_rent} has increased to {new_rent}.\nThe Status:{prop_status2}")
