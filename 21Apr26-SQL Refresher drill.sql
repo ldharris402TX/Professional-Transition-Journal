8725d4bf4102fabde6f57f03fd5c7353214a0158
LaKendra D Randolph <kendra@lakendras-air.lan>
2026-04-21 17:22:33 -0500

    Phase 2-Lessons and SQL drill

---
+++ 21Apr26-SQL Refresher drill.sql
@@ -0,0 +1,10 @@
+#Identifying all delinquent accts so the user access can be suspended
+
+SELECT c.first_name,c.last_name,r.rental_date FROM customer AS c
+INNER JOIN rental AS r
+ON c.customer_id = r.customer_id
+WHERE r.return_date IS NULL
+ORDER BY r.rental_date ASC
+
+Phase 2-Cloud and Security
+AWS IAM(Identity and Access Management) lesson
